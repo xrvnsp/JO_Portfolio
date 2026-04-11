1b0b70ada1fa9a8076e119c0eee3195f75373193
Saravana <saravana.r@digifox.studio>
2026-04-11 12:58:33 +0530

    Fix white screen on Vercel by setting base path to root

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/JO_Portfolio/',
+  base: '/',
   plugins: [react()],
   build: {
     cssTarget: 'chrome80'
